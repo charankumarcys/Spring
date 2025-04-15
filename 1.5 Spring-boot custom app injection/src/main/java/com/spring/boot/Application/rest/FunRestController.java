9187b87123e19bce79f5fdc6f8a2fa0f7e16b47e
unknown <charankumarcys@gmail.com>
2025-04-15 16:10:14 +0530

    Custom Injection

---
+++ 1.5 Spring-boot custom app injection/src/main/java/com/spring/boot/Application/rest/FunRestController.java
@@ -0,0 +1,36 @@
+package com.spring.boot.Application.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String teaminfo(){
+        return "coachname: "+ coachName +"teamname: "+teamName;
+    }
+
+    @GetMapping("/url")
+    public String getMsg(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/greetings")
+    public String greeting(){
+        return "Good Morning! Have a good day!!";
+    }
+
+    @GetMapping("/trending")
+    public String trend(){
+        return "Trending!!!";
+    }
+
+}
